13ce8cd29a19e58fb2bec7a0004a2b289554cca6
Sam Boyd <samboyd10@gmail.com>
2022-03-05 09:14:44 +0000

    Sort exercises by category in the pick exercises page

--- app/src/pages/pickExercise.js
+++ app/src/pages/pickExercise.js
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import {PAGE} from '../constants'
 import {pickExerciseForTemplateWorkoutAction, pickExerciseForWorkoutAction} from "../reducers/actions"
 import {selectUiState} from "../reducers/uiStateReducer";
-import {selectExercises} from "../reducers/exercisesReducer";
+import {removeExercise, selectExercises} from "../reducers/exercisesReducer";
+import styled from "styled-components";
 
 const PickExercise = () => {
     const uiState = useSelector(selectUiState)
@@ -19,14 +20,54 @@ const PickExercise = () => {
         }
     }
 
+    const CategoryHeader = styled.div`
+    
+`
+
+    const CategoryExerciseList = styled.div`
+    margin: auto;
+`
+
+    const CategoryExerciseListItem = styled.div`
+    margin: auto;
+    font-weight: lighter;
+`
+
+    const Category = (props) => {
+        const dispatch = useDispatch()
+
+        if (props.exercises.length === 0) {
+            return <></>
+        }
+
+        const removeExerciseId = exerciseId => () => {
+            dispatch(removeExercise({id: exerciseId}))
+        }
+
+        return (
+            <div>
+                <CategoryHeader>{props.name}</CategoryHeader>
+                <CategoryExerciseList>
+                    {props.exercises.map((ex, i) => {
+                        return <CategoryExerciseListItem key={i} onClick={selectExercise(ex)}>
+                            {ex.name}
+                        </CategoryExerciseListItem>
+                    })}
+                </CategoryExerciseList>
+            </div>
+        )
+    }
+
+    const sortedExercises = {}
+    exercises.categories.forEach(cat => {
+        sortedExercises[cat] = exercises.exercises.filter(e => e.category === cat)
+    })
+
+
     return (
         <div>
             <p>Pick an exercise</p>
-            <ul>
-                { exercises.exercises.map(
-                    (exercise, index) => <li key={index} onClick={selectExercise(exercise)}>{exercise.name}</li>
-                ) }
-            </ul>
+            {Object.keys(sortedExercises).map((cat, i) => <Category key={i} name={cat} exercises={sortedExercises[cat]}/>)}
         </div>
     )
 }
