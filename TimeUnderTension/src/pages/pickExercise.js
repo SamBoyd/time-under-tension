60c8c7c2098d319119f38df18fe3c8d48fdf9abb
Sam Boyd <samboyd10@gmail.com>
2022-03-21 08:33:18 +0000

    Styling the add work page

--- TimeUnderTension/src/pages/pickExercise.js
+++ TimeUnderTension/src/pages/pickExercise.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { useDispatch, useSelector } from "react-redux";
+import React, {useState} from 'react'
+import {useDispatch, useSelector} from "react-redux";
 
 import {PAGE} from '../constants'
 import {pickExerciseForTemplateWorkoutAction, pickExerciseForWorkoutAction} from "../reducers/actions"
@@ -11,20 +11,26 @@ import {
     selectUiState
 } from "../reducers/uiStateReducer";
 import {selectExercises} from "../reducers/exercisesReducer";
-import {StyleSheet, Text, View} from "react-native";
+import {Dimensions, StyleSheet, Text, View} from "react-native";
 import SectionList from "react-native/Libraries/Lists/SectionList";
 import {TextBold, TextH1, TextNormal} from "../components/styled/text";
 import {Button} from "../components/styled/button";
-import {ListItem} from "react-native-elements";
+import {Avatar, Icon, ListItem} from "react-native-elements";
 import Header from "../components/header";
 import theme from "../theme";
 import {capitalizeFirstLetter} from "../utils/textUtils";
+
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 
 const PickExercise = () => {
     const uiState = useSelector(selectUiState)
     const exercises = useSelector(selectExercises)
     const dispatch = useDispatch()
+
+    const [expandedSection, setExpandedSection] = useState('')
 
     const selectExercise = exercise => () => {
         if (uiState.redirectTo === PAGE.workout) {
@@ -43,7 +49,7 @@ const PickExercise = () => {
         sortedExercises.push({'title': cat, 'data': exercises.exercises.filter(e => e.category === cat)})
     })
 
-    const Item = ({ exercise }) => (
+    const Item = ({exercise}) => (
         <ListItem>
             <ListItem.Content>
                 <TextNormal onPress={selectExercise(exercise)}>{exercise.name}</TextNormal>
@@ -51,14 +57,25 @@ const PickExercise = () => {
         </ListItem>
     );
 
+    const toggleExpandSection = section => () => {
+        if (expandedSection === section) {
+            setExpandedSection('')
+        } else {
+            setExpandedSection(section)
+        }
+    }
+
+
     const styles = StyleSheet.create({
         wrapper: {
-          backgroundColor: theme.colors.tertiary
+            backgroundColor: theme.colors.tertiary,
+            height: windowHeight,
+            width: windowWidth,
+
         },
 
-        backButton: {
 
-        },
+        backButton: {},
 
         header: {
             title: {
@@ -68,17 +85,25 @@ const PickExercise = () => {
 
         listWrapper: {
             backgroundColor: theme.colors.tertiary,
-            margin: 40,
         },
 
         listHeader: {
             backgroundColor: theme.colors.tertiary,
+            paddingLeft: 20,
+            paddingTop: 10,
+            paddingBottom: 5,
         },
         listItem: {
             backgroundColor: theme.colors.tertiary,
-            paddingRight: 20,
+            paddingLeft: 40,
+            paddingTop: 5,
+            paddingBottom: 5,
+            text: {
+              fontSize: 12,
+            },
         },
     })
+
 
     return (
         <View style={styles.wrapper}>
@@ -87,19 +112,33 @@ const PickExercise = () => {
                 centerComponent={<TextH1 style={styles.header.title}>Pick an exercise</TextH1>}
             />
 
-            <View syles={styles.listWrapper}>
-                <SectionList
-                    sections={sortedExercises}
-                    keyExtractor={exercise => exercise.id}
-                    renderItem={({item}) => <Item exercise={item} />}
-                    renderSectionHeader={({ section: { title } }) => (
-                        <ListItem topDivider>
-                            <ListItem.Subtitle>
-                                <TextBold>{capitalizeFirstLetter(title)}</TextBold>
-                            </ListItem.Subtitle>
-                        </ListItem>
-                    )}
-                />
+            <View syle={styles.listWrapper}>
+                {sortedExercises.map(category => {
+                    return <ListItem.Accordion
+                        noRotation
+                        content={
+                            <ListItem.Content>
+                                <ListItem.Title>{capitalizeFirstLetter(category.title)}</ListItem.Title>
+                            </ListItem.Content>
+                        }
+                        isExpanded={expandedSection === category.title}
+                        onPress={toggleExpandSection(category.title)}
+                        containerStyle={styles.listHeader}
+                    >
+                        {category.data.map((item, i) => {
+                            return <ListItem
+                                key={i}
+                                onPress={selectExercise(item)}
+                                bottomDivider
+                                containerStyle={styles.listItem}
+                            >
+                                <ListItem.Content >
+                                    <ListItem.Title style={styles.listItem.text}>{item.name}</ListItem.Title>
+                                </ListItem.Content>
+                            </ListItem>
+                        })}
+                    </ListItem.Accordion>
+                })}
             </View>
         </View>
     )
