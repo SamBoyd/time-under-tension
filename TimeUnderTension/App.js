7dc1c7bf36a613c917b16a7eeb70c8374522f074
Sam <samboyd10@gmail.com>
2022-03-28 19:06:25 +0100

    Added redux-persist to persist the redux store

--- TimeUnderTension/App.js
+++ TimeUnderTension/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import type {Node} from 'react';
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 import {
     useColorScheme, View,
 } from 'react-native';
 
 import {SafeAreaProvider} from "react-native-safe-area-context/src/SafeAreaContext";
 import {ThemeProvider} from "react-native-elements";
+
 import {
     Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
 import MainApp from './src/App'
-import store from "./src/store";
+import store, {persistor} from "./src/store";
 import theme from './src/theme'
 
 const App = () => {
@@ -25,11 +27,13 @@ const App = () => {
     return (
         <React.StrictMode>
             <Provider store={store}>
-                <SafeAreaProvider>
-                    <ThemeProvider theme={theme}>
-                        <MainApp/>
-                    </ThemeProvider>
-                </SafeAreaProvider>
+                <PersistGate loading={null} persistor={persistor}>
+                    <SafeAreaProvider>
+                        <ThemeProvider theme={theme}>
+                            <MainApp/>
+                        </ThemeProvider>
+                    </SafeAreaProvider>
+                </PersistGate>
             </Provider>
         </React.StrictMode>
     );
--- TimeUnderTension/src/store.js
+++ TimeUnderTension/src/store.js
@@ -1,22 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+import devToolsEnhancer from 'remote-redux-devtools';
+import {persistReducer, persistStore} from "redux-persist";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import workoutReducer from './reducers/workoutReducer'
 import uiStateReducer from "./reducers/uiStateReducer";
 import timerReducer from "./reducers/timerReducer";
 import templateWorkoutReducer from "./reducers/templateWorkoutReducer";
 import historyReducer from "./reducers/historyReducer";
 import exercisesReducer from "./reducers/exercisesReducer";
-import devToolsEnhancer from 'remote-redux-devtools';
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE} from "redux-persist/es/constants";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 
-export default configureStore({
-    reducer: {
-        workout: workoutReducer,
-        uiState: uiStateReducer,
-        timer: timerReducer,
-        templateWorkouts: templateWorkoutReducer,
-        history: historyReducer,
-        exercises: exercisesReducer
-    },
+
+const rootReducer = combineReducers({
+    workout: workoutReducer,
+    uiState: uiStateReducer,
+    timer: timerReducer,
+    templateWorkouts: templateWorkoutReducer,
+    history: historyReducer,
+    exercises: exercisesReducer,
+})
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage: AsyncStorage,
+    stateReconciler: autoMergeLevel2
+}
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedRootReducer,
     devTools: false,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
     enhancers: [devToolsEnhancer({
         name: 'Android app', realtime: true,
         hostname: 'localhost', port: 8000,
@@ -24,8 +47,12 @@ export default configureStore({
         secure: false,
         actionSanitizer: (action) => (
             action.type === 'FILE_DOWNLOAD_SUCCESS' && action.data ?
-                { ...action, data: '<<LONG_BLOB>>' } : action
+                {...action, data: '<<LONG_BLOB>>'} : action
         ),
-        stateSanitizer: (state) => state.data ? { ...state, data: '<<LONG_BLOB>>' } : state
+        stateSanitizer: (state) => state.data ? {...state, data: '<<LONG_BLOB>>'} : state
     })]
 })
+
+export const persistor = persistStore(store)
+
+export default store
