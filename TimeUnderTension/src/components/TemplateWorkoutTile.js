fb2c983a5acd15a306b2ce7211b41fc2e269eedc
Sam Boyd <samboyd10@gmail.com>
2022-04-03 10:14:52 +0100

    Bug preventing workouts being created from templates

--- TimeUnderTension/src/components/TemplateWorkoutTile.js
+++ TimeUnderTension/src/components/TemplateWorkoutTile.js
@@ -70,7 +70,7 @@ const TemplateWorkoutTile = props => {
     }
 
     const startWorkoutFromTemplate = () => {
-        dispatch(createWorkoutFromTemplate(template))
+        dispatch(createWorkoutFromTemplate(props.template))
         props.moveToWorkout()
     }
 
