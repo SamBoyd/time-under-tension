a7d64f25b818e9761ce0447ae87cacfb6965ae21
Sam <samboyd10@gmail.com>
2022-04-28 08:42:55 +0100

    Add multiple work at once

--- TimeUnderTension/src/pages/pickExercise.js
+++ TimeUnderTension/src/pages/pickExercise.js
@@ -4,10 +4,10 @@ import {useDispatch, useSelector} from "react-redux";
 import {PAGE} from '../constants'
 import {addSetAction} from "../reducers/actions"
 import {selectExercises} from "../reducers/exercisesReducer";
-import {Dimensions, StyleSheet} from "react-native";
+import {Dimensions, Pressable, StyleSheet, View} from "react-native";
 import {TextNormal} from "../components/styled/text";
 import {Button} from "../components/styled/button";
-import {ListItem} from "react-native-elements";
+import {Divider, ListItem} from "react-native-elements";
 import theme, {standardHorizontalPadding, standardVerticalPadding} from "../theme";
 import {capitalizeFirstLetter} from "../utils/textUtils";
 import BasePage from "../components/basePage";
@@ -16,8 +16,8 @@ import {addWork, newWorkForExercise} from "../reducers/workReducer";
 import {addWork as addWorkToWorkout} from "../reducers/workoutReducer";
 import {addWork as addWorkToTemplate} from "../reducers/newTemplateWorkoutReducer";
 import {setActiveWorkIfUndefined} from "../reducers/timerReducer";
-import {FlexRowView} from "../components/styled/view";
-
+import {FlexColumnView, FlexRowView} from "../components/styled/view";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -32,6 +32,7 @@ const PickExercise = ({route, navigation}) => {
     const dispatch = useDispatch()
 
     const [expandedSection, setExpandedSection] = useState('')
+    const [exercisesToSave, setExercisesToSave] = useState([])
 
     const {saveWorkTo} = route.params
 
@@ -42,7 +43,6 @@ const PickExercise = ({route, navigation}) => {
         addSetAction(dispatch, newWork.id)
         addSetAction(dispatch, newWork.id)
         addSetAction(dispatch, newWork.id)
-        navigation.goBack()
     }
 
     const saveToWorkout = exercise => {
@@ -53,14 +53,39 @@ const PickExercise = ({route, navigation}) => {
         addSetAction(dispatch, newWork.id)
         addSetAction(dispatch, newWork.id)
         dispatch(setActiveWorkIfUndefined(newWork.id))
+    }
+
+    const saveExercisesToTemplate = () => {
+        exercisesToSave.forEach(ex => {
+            saveToTemplate(ex)
+        })
+        navigation.goBack()
+    }
+
+    const saveExercisesToWorkout = () => {
+        exercisesToSave.forEach(ex => {
+            console.log(`Save exercise: ${JSON.stringify(ex)}`)
+            saveToWorkout(ex)
+        })
         navigation.goBack()
     }
 
     const selectExercise = exercise => () => {
+        const newExerciseList = [...exercisesToSave, exercise]
+        setExercisesToSave(newExerciseList)
+    }
+
+    const deselectExercise = exercise => () => {
+        let newExerciseList = [...exercisesToSave, exercise]
+        newExerciseList = newExerciseList.filter(ex => ex.id !== exercise.id)
+        setExercisesToSave(newExerciseList)
+    }
+
+    const submit = () => {
         if (saveWorkTo === SAVE_WORK_TO.template) {
-            saveToTemplate(exercise)
+            saveExercisesToTemplate()
         } else {
-            saveToWorkout(exercise)
+            saveExercisesToWorkout()
         }
     }
 
@@ -77,14 +102,6 @@ const PickExercise = ({route, navigation}) => {
         sortedExercises.push({'title': cat, 'data': exercises.exercises.filter(e => e.category === cat)})
     })
 
-    const Item = ({exercise}) => (
-        <ListItem>
-            <ListItem.Content>
-                <TextNormal onPress={selectExercise(exercise)}>{exercise.name}</TextNormal>
-            </ListItem.Content>
-        </ListItem>
-    );
-
     const toggleExpandSection = section => () => {
         if (expandedSection === section) {
             setExpandedSection('')
@@ -95,6 +112,18 @@ const PickExercise = ({route, navigation}) => {
 
 
     const styles = StyleSheet.create({
+
+        actionWrapper: {
+            paddingVertical: hp(1),
+            justifyContent: 'space-between',
+            paddingBottom: standardVerticalPadding,
+
+        },
+
+        saveWrapper: {
+            width: wp(30),
+            justifyContent: 'space-between',
+        },
 
         listWrapper: {
             backgroundColor: theme.colors.tertiary,
@@ -118,15 +147,72 @@ const PickExercise = ({route, navigation}) => {
                 color: theme.colors.white,
             },
         },
+
+        selected: {
+            container: {
+                rowGap: hp(1),
+                marginTop: hp(3),
+                paddingBottom: standardVerticalPadding,
+            },
+
+            row: {
+                marginLeft: wp(5),
+                // verticalAlign: 'center',
+            },
+
+            removeIcon: {
+                ...theme.Icon
+            },
+
+            iconWrapper: {
+                // marginHorizontal: wp(1),
+                justifyContent: 'center',
+            },
+
+            nameWrapper: {
+                paddingHorizontal: wp(3),
+                justifyContent: 'center',
+            },
+
+            name: {}
+        }
     })
 
 
     return (
         <BasePage>
-            <FlexRowView>
+            <FlexRowView viewStyle={styles.actionWrapper}>
                 <Button onPress={goBack} title="back" containerStyle={styles.backButton}/>
-                <Button onPress={goNewExercise} title="New" containerStyle={styles.backButton} testId='newExerciseButton'/>
+                <FlexRowView viewStyle={styles.saveWrapper}>
+                    <Button onPress={goNewExercise} title="New" containerStyle={styles.backButton}
+                            testId='newExerciseButton'/>
+                    <Button onPress={submit} title="Save"/>
+                </FlexRowView>
             </FlexRowView>
+
+            <Divider/>
+
+            {exercisesToSave.length > 0 && (
+                <>
+                    <FlexColumnView viewStyle={styles.selected.container} rowGap={styles.selected.container.rowGap}>
+                        {exercisesToSave.map(ex =>
+                            <FlexRowView viewStyle={styles.selected.row}>
+                                <Pressable onPress={deselectExercise(ex)}>
+                                    <View style={styles.selected.iconWrapper}>
+                                        <MaterialIcons name="highlight-remove" {...styles.selected.removeIcon}/>
+                                    </View>
+                                </Pressable>
+                                <View style={styles.selected.nameWrapper}>
+                                    < TextNormal style={styles.selected.name}>{ex.name}</TextNormal>
+                                </View>
+                            </FlexRowView>
+                        )}
+                    </FlexColumnView>
+
+                    <Divider/>
+                </>
+            )}
+
 
             {sortedExercises.map((category, ii) => {
                 return <ListItem.Accordion
@@ -134,7 +220,8 @@ const PickExercise = ({route, navigation}) => {
                     noRotation
                     content={
                         <ListItem.Content>
-                            <ListItem.Title style={styles.listHeader.text}>{capitalizeFirstLetter(category.title)}</ListItem.Title>
+                            <ListItem.Title
+                                style={styles.listHeader.text}>{capitalizeFirstLetter(category.title)}</ListItem.Title>
                         </ListItem.Content>
                     }
                     isExpanded={expandedSection === category.title}
