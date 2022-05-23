be845609e37fec8f755cb1734d2bdd6fdac324e4
Sam <samboyd10@gmail.com>
2022-05-23 07:59:45 +0100

    Added placeholder page for analytics to be able to release without that page

--- TimeUnderTension/src/pages/analytics.js
+++ TimeUnderTension/src/pages/analytics.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import {View} from "react-native";
+import {StyleSheet, View} from "react-native";
+import {TextNormal} from "../components/styled/text";
+import BasePage from "../components/basePage";
+import {standardHorizontalPadding, standardVerticalPadding} from "../theme";
 
 const Analytics = props => {
 
+    const styles = StyleSheet.create({
+        wrapper: {
+            marginTop: standardVerticalPadding,
+            marginHorizontal: standardHorizontalPadding,
+        }
+    })
     return (
-        <View></View>
+        <BasePage>
+            <View style={styles.wrapper}>
+                <TextNormal>Analytics coming soon...</TextNormal>
+            </View>
+        </BasePage>
     )
 }
 
